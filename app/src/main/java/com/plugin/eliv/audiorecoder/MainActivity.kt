d52689eb83118f3910a26bfe781e686aaa92c3fd
spmno <sunqingpeng@hotmail.com>
2024-12-24 14:02:33 +0800

    add file path

--- app/src/main/java/com/plugin/eliv/audiorecoder/MainActivity.kt
+++ app/src/main/java/com/plugin/eliv/audiorecoder/MainActivity.kt
@@ -1,19 +1,23 @@
 package com.plugin.eliv.audiorecoder
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
 import com.plugin.eliv.recoderlibrary.AudioEngine
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    private val REQUEST_CODE = 1024
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        requestPermission()
         AudioEngine.getInstance().enableSystemAEC().init(this)
+        val waveFilePath = getExternalFilesDir(null)?.absolutePath + "/wave.wav";
 
         recode.setOnClickListener {
-            AudioEngine.getInstance().startRecord("[path]")
+            AudioEngine.getInstance().startRecord(waveFilePath)
         }
 
         stopRecode.setOnClickListener {
@@ -21,11 +25,21 @@ class MainActivity : AppCompatActivity() {
         }
 
         play.setOnClickListener {
-            AudioEngine.getInstance().startPlay("[path]")
+            AudioEngine.getInstance().startPlay(waveFilePath)
         }
 
         stopPlay.setOnClickListener {
             AudioEngine.getInstance().stopPlay()
+        }
+    }
+
+    private fun requestPermission() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            } else {
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE, android.Manifest.permission.WRITE_EXTERNAL_STORAGE), REQUEST_CODE)
+            }
         }
     }
 }
